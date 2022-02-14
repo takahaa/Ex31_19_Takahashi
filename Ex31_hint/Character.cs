735a7835793059dd6890d2c9a445d96ea0f75532
Jun Shimura <junshimura@gmail.com>
2022-02-14 14:48:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Ex31_hint/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex31_hint
+{
+    class Character
+    {
+        readonly public string name;
+        public float hp;
+        protected float power;
+        public Character(string name,float hp,float power)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.power = power;
+        }
+        public virtual void Attack(Character destination)
+        {
+            Console.WriteLine($"{this.name}は{destination.name}をぶんなぐった！");
+            Console.WriteLine($"{destination.name}は{this.power}のダメージ");
+            destination.hp -= this.power;
+            Console.WriteLine($"{destination.name}の残りのHPは{destination.hp}");
+        }
+    }
+}
---
+++ Ex31_hint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex31_hint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character1 = new Character("しむら人", 500, 2);
+            Character character2 = new Character("ささっき", 1000, 25);
+            Tank tank1 = new Tank("Tiger", 500000, 250);
+            tank1.bullet = 10;
+            Tank tank2 = new Tank("がんタンク", 10000, 125);
+            tank2.bullet = 5;
+            Character[] characters = { character1, character2, tank1, tank2 };
+            for(var i = 1; i < characters.Length; i++)
+            {
+                characters[i].Attack(character1);
+            }
+        }
+    }
+}
